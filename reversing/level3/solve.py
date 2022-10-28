df25a12eeaebeac0a1ad9338a6b4fc06ecbe415f
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2022-10-28 15:48:38 +0200

    update offset

--- reversing/level3/solve.py
+++ reversing/level3/solve.py
@@ -2,8 +2,8 @@
 import angr
 
 p = angr.Project("level3")
-good = 0x01012a1
-bad = 0x001012a
+good = 0x01012a1 + 0x400000
+bad = 0x001012a + 0x400000
 
 sm = p.factory.simulation_manager()
 print(sm.explore(find=good, avoid=bad))
